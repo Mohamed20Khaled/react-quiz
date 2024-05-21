94ed05988ceaa1bbe28e6bf866e13f0e29def555
Mohamed20Khaled <mk740015@gmail.com>
2024-05-21 18:02:24 +0300

    finishing a quiz

---
+++ src/components/FinishScreen.js
@@ -0,0 +1,22 @@
+function FinishScreen({ points, maxPoints, highscore }) {
+  const percentage = (points / maxPoints) * 100;
+
+  let emoji;
+  if (percentage === 100) emoji = "🎖️";
+  if (percentage >= 80 && percentage < 100) emoji = "🎉";
+  if (percentage >= 50 && percentage < 80) emoji = "😊";
+  if (percentage >= 0 && percentage < 50) emoji = "😳";
+  if (percentage === 0) emoji = "🤦";
+
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span> You Scored <strong>{points}</strong> out of{" "}
+        {maxPoints} ({Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">(Highscore: {highscore} points)</p>
+    </>
+  );
+}
+
+export default FinishScreen;
